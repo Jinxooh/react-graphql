8fc59e0851951ae7e79b7e94d845919c7d54d575
황진수-PC <cjswp122@gmail.com>
2018-07-13 11:03:14 +0900

    implemented react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import client from './client';
+import Home from './Home';
+import Detail from './Detail';
 class App extends Component {
   render() {
     return (<ApolloProvider client={client}>
-        <div className="App" />
+        <Router>
+          <React.Fragment>
+            <Route exact={true} path={"/"} component={Home}></Route>
+            <Route exact={true} path={"/Detail"} component={Detail}></Route>
+          </React.Fragment>
+        </Router>
       </ApolloProvider>
     );
   }
